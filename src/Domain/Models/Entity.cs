0525b6c8daef3bf6950615a8b62f48db4f5c3636
Michael Trullas Garcia <adoser2@hotmail.es>
2023-06-06 00:02:07 +0200

    WIP IAudited

--- src/Domain/Models/Entity.cs
+++ src/Domain/Models/Entity.cs
@@ -3,9 +3,13 @@ using Michael.Net.Domain;
 
 namespace Domain.Models
 {
-    public abstract class Entity : IEntity
+    public abstract class Entity : IEntity, IAudited
     {
         public long Id { get; set; }
         [Write(false)] public Guid Guid { get; set; }
+        public long CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public long? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
     }
 }
