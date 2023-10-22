4dd87e693be35ccd9b95c39c64933345a5b3695a
Michael Trullas Garcia <adoser2@hotmail.es>
2023-10-22 17:03:58 +0200

    WIP

--- src/Domain/Models/Entity.cs
+++ src/Domain/Models/Entity.cs
@@ -8,8 +8,8 @@ namespace Domain.Models
         public long Id { get; set; }
         [Write(false)] public Guid Guid { get; set; }
         public long CreatedBy { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedOn { get; set; }
         public long? ModifiedBy { get; set; }
-        public DateTime? ModifiedDate { get; set; }
+        public DateTime? ModifiedOn { get; set; }
     }
 }
