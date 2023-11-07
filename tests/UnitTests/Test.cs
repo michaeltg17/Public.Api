f9db5266af7390c1d3b8723f6e552d5fd7dabb16
Michael Trullas Garcia <adoser2@hotmail.es>
2023-11-07 14:14:36 +0100

    WIP

---
+++ tests/UnitTests/Test.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace UnitTests
+{
+    public class Test
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            (1 + 1).Should().Be(2);
+        }
+    }
+}
