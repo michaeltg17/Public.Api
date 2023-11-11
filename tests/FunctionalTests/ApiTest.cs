b204b9ad463b5578155aaab205129644f2dbd0c1
Michael Trullas Garcia <adoser2@hotmail.es>
2023-11-11 20:39:31 +0100

    WIP

--- tests/FunctionalTests/ApiTest.cs
+++ tests/FunctionalTests/ApiTest.cs
@@ -6,7 +6,7 @@
 
     public static class Routes
     {
-        public const string ApiUrl = "https://localhost:5001";
+        public const string ApiUrl = "http://localhost:80";
         public const string SaveImageGroup = ApiUrl + "/SaveImageGroup";
         public const string GetImageGroup = ApiUrl + "/GetImageGroup";
         public const string GetImage = ApiUrl + "/GetImage";
