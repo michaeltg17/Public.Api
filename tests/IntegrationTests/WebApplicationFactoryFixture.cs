8ca7d6e4770b2b13a67590448243cf89aebbe160
Michael Trullas Garcia <adoser2@hotmail.es>
2023-11-23 21:26:55 +0100

    WIP

--- tests/IntegrationTests/WebApplicationFactoryFixture.cs
+++ tests/IntegrationTests/WebApplicationFactoryFixture.cs
@@ -4,12 +4,17 @@ using Microsoft.Extensions.Hosting;
 using Serilog;
 using Xunit;
 using Xunit.Abstractions;
+using Testcontainers.MsSql;
+using Microsoft.Extensions.DependencyInjection;
+using CrossCutting;
 
 namespace IntegrationTests
 {
     public class WebApplicationFactoryFixture : WebApplicationFactory<Program>, IAsyncLifetime
     {
         public ITestOutputHelper TestOutputHelper { get; set; } = default!;
+
+        readonly MsSqlContainer _sqlServerContainer = new MsSqlBuilder().Build();
 
         public Task InitializeAsync()
         {
@@ -24,11 +29,14 @@ namespace IntegrationTests
                 configuration.WriteTo.TestOutput(TestOutputHelper);
             });
 
+            builder.ConfigureServices(s => s.Configure<Settings>(c => c.SqlServerConnectionString = _sqlServerContainer.GetConnectionString()));
+
             return base.CreateHost(builder);
         }
 
         public new Task DisposeAsync()
         {
+            _sqlServerContainer.DisposeAsync();
             return base.DisposeAsync().AsTask();
         }
     }
