b87cf191be132b6c3532091ab169f0c30017cb0c
Michael Trullas Garcia <adoser2@hotmail.es>
2024-04-13 11:51:25 +0200

    Add test for API Client To method

---
+++ tests/IntegrationTests/Tests/ApiClientTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Xunit.Abstractions;
+using Xunit;
+using ApiClient.Extensions;
+using Domain.Models;
+using ApiClient.Exceptions;
+
+namespace IntegrationTests.Tests
+{
+    [Collection("ApiCollection")]
+    public class ApiClientTests(ITestOutputHelper testOutputHelper, WebApplicationFactoryFixture factory) 
+        : Test(testOutputHelper, factory)
+    {
+        [Fact]
+        public async Task WhenInternalServerError_ApiExceptionIsThrownWithExpectedProblemDetails()
+        {
+            //When
+            var response = await apiClient.ThrowInternalServerError();
+
+            //Then
+            var expectedMessage =
+                "{\r\n" +
+                "  \"type\": \"https://tools.ietf.org/html/rfc9110#section-15.6.1\",\r\n" +
+                "  \"title\": \"InternalServerError\",\r\n" +
+                "  \"status\": 500,\r\n" +
+                "  \"detail\": \"Internal server error. Please contact the API support.\",\r\n" +
+                "  \"instance\": \"/Test/ThrowInternalServerError\"\r\n" +
+                "}";
+
+            var func = async () => await response.To<ImageGroup>();
+            await func.Should().ThrowAsync<ApiException>().WithMessage(expectedMessage);
+        }
+    }
+}
