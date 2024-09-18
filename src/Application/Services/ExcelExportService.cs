7ab1f5c0da8ab6c1871d887120a98006ad2ca9c4
Michael Trullas Garcia <adoser2@hotmail.es>
2024-09-18 12:29:16 +0200

    WIP

---
+++ src/Application/Services/ExcelExportService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence;
+using SpreadCheetah;
+
+namespace Application.Services
+{
+    internal class ExcelExportService(AppDbContext db)
+    {
+        public async Task<byte[]> Export(string tableName)
+        {
+            //Try do the same with dapper
+            //Fix sql injection
+            var query = $"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = {tableName}";
+            var columns = await db.Set<object>().FromSqlRaw(query).ToListAsync();
+
+            query = $"SELECT * FROM {tableName}";
+            var rows = await db.Set<object>().FromSqlRaw(query).ToListAsync();
+
+            using var stream = new MemoryStream();
+            using var spreadsheet = await Spreadsheet.CreateNewAsync(stream);
+            await spreadsheet.StartWorksheetAsync(tableName);
+
+            // Cells are inserted row by row.
+            var row = new List<Cell>();
+            foreach(var @object in rows)
+            {
+                row.Add()
+            }
+            {
+                new Cell("Answer to the ultimate question:"),
+                new Cell(42)
+            };
+
+            // Rows are inserted from top to bottom.
+            await spreadsheet.AddRowAsync(row);
+
+            // Remember to call Finish before disposing.
+            // This is important to properly finalize the XLSX file.
+            await spreadsheet.FinishAsync();
+        }
+    }
+}
