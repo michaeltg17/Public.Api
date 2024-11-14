9f5bb887691d7233dfa59aed3616427b811d7afa
Michael Trullas <adoser2@hotmail.es>
2024-11-14 13:33:22 +0100

    WIP

--- tests/IntegrationTests/Tests/ApiClient/ApiClientTests.cs
+++ tests/IntegrationTests/Tests/ApiClient/ApiClientTests.cs
@@ -19,14 +19,18 @@ namespace IntegrationTests.Tests.ApiClient
             var response = await ApiClient.GetTestEndpoints(apiType).ThrowInternalServerError();
 
             //Then
-            var expectedMessage =
-                "{\r\n" +
-                "  \"type\": \"https://tools.ietf.org/html/rfc9110#section-15.6.1\",\r\n" +
-                "  \"title\": \"InternalServerError\",\r\n" +
-                "  \"status\": 500,\r\n" +
-                "  \"detail\": \"Internal server error. Please contact the API support.\",\r\n" +
-                "  \"instance\": \"/" + apiType + "/ThrowInternalServerError\"\r\n" +
-                "}";
+            var problemDetails = await response.To<ProblemDetails>();
+
+            var expectedMessage = $$"""
+                {
+                  "type": "https://tools.ietf.org/html/rfc9110#section-15.6.1",
+                  "title": "InternalServerError",
+                  "status": 500,
+                  "detail": "Internal server error. Please contact the API support.",
+                  "instance": "/{{apiType}}/ThrowInternalServerError",
+                  "traceId": "{{problemDetails.Extensions["traceId"]}}"
+                }
+                """;
 
             var func = response.To<ImageGroup>;
             await func.Should().ThrowAsync<ApiException>().WithMessage(expectedMessage);
