0bc9f323a16d672a3d95cbb65442eb44d4a38218
Michael Trullas <adoser2@hotmail.es>
2024-11-22 01:26:08 +0100

    WIP

---
+++ src/Domain/ValueObjects/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.ValueObjects
+{
+    public readonly record struct Address(
+        string FirstName,
+        string LastName,
+        string Street,
+        string PostalCode,
+        string City, 
+        string State,
+        string Country);
+}
---
+++ src/Domain/ValueObjects/CreditCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Domain.ValueObjects
+{
+    public readonly record struct CreditCard(
+        string CardNumber,
+        string CardHolderName,
+        DateTime ExpirationDate,
+        string SecurityCode)
+    {
+        public string CardNumber { get; private set; } = "";
+    }
+}
